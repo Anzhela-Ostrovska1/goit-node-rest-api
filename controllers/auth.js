98e03d541e5a62e19be1446aa52a11d7067f7571
Anzhela Ostrovska <anzhelk451964@gmail.com>
2024-03-12 14:35:58 -0700

    changed avatars

--- controllers/auth.js
+++ controllers/auth.js
@@ -88,7 +88,7 @@ const updateAvatar = async (req, res) => {
   const resultUpload = path.join(avatarsDir, filename);
 
   const image = await Jimp.read(tempUpload);
-  await image.resize(250, 250).write(resultUpload);
+  await image.resize(250, 250).writeAsync(resultUpload);
   await fs.unlink(tempUpload);
 
   const avatarURL = path.join("avatars", filename);
